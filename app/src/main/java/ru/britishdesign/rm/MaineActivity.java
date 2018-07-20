0d7d60e62a4991dfd9eb5e6fdc28147e341b43cd
donyab <t303t@ya.ru>
2018-07-20 18:52:36 +0300

    Main activity

---
+++ app/src/main/java/ru/britishdesign/rm/MaineActivity.java
@@ -0,0 +1,16 @@
+package ru.britishdesign.rm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MaineActivity extends Activity {
+
+@Override
+    protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+
+}
+
+}
